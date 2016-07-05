f7ea45c7fa8b56dc4401d3a937c154f910637d9c
Joshua Yang <tianjyang@gmail.com>
2016-07-05 22:16:11 +0000

    uninstalled ar-extensions

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -2,18 +2,18 @@ class Incident < ActiveRecord::Base
 has_many :dbags
 
   private
-  def self.seed
-    require 'csv'
-    file = File.dirname(__FILE__) + "/incident_seed.csv"
-    csv_text = File.read(file)
-    csv = CSV.parse(csv_text, :headers => true)
-    csv.each do |x|
-    hash = {}
-    hash[:name]=x[0]
-    hash[:descrip]=x[1]
-    a = self.new(hash)
-    a.save
-    end
-  end
+  # def self.seed
+  #   require 'csv'
+  #   file = File.dirname(__FILE__) + "/incident_seed.csv"
+  #   csv_text = File.read(file)
+  #   csv = CSV.parse(csv_text, :headers => true)
+  #   csv.each do |x|
+  #   hash = {}
+  #   hash[:name]=x[0]
+  #   hash[:descrip]=x[1]
+  #   a = self.new(hash)
+  #   a.save
+  #   end
+  # end
 end
 
--- app/models/make.rb
+++ app/models/make.rb
@@ -2,16 +2,16 @@ class Make < ActiveRecord::Base
   has_many :dbags
   
   private
-  def self.seed
-    require 'csv'
-    file = File.dirname(__FILE__) + "/makes_seed.csv"
-    csv_text = File.read(file)
-    csv = CSV.parse(csv_text, :headers => true)
-    csv.each do |x|
-    hash = {}
-    hash[:name]=x[0]
-    a = self.new(hash)
-    a.save
-    end
-  end
+  # def self.seed
+  #   require 'csv'
+  #   file = File.dirname(__FILE__) + "/makes_seed.csv"
+  #   csv_text = File.read(file)
+  #   csv = CSV.parse(csv_text, :headers => true)
+  #   csv.each do |x|
+  #   hash = {}
+  #   hash[:name]=x[0]
+  #   a = self.new(hash)
+  #   a.save
+  #   end
+  # end
 end
